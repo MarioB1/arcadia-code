44e3f24327a5f28873aa94e746146fd6fd752b05
MarioB1 <mcbruckert@gmail.com>
2024-10-23 15:47:07 +0000

    habitat fait

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,8 @@ export const allRoutes = [
     new Route("/services", "Nos services", "/pages/services.html"),
     new Route("/habitats", "Nos habitats", "/pages/habitats.html"),
     new Route("/jungle", "Jungle", "/pages/jungle.html"),
+    new Route("/marais", "Marais", "/pages/marais.html"),
+    new Route("/savane", "Savane", "/pages/savane.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
